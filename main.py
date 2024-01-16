b954e8c2fa214af2d77b6c706966be3a4647d0b2
Tuan Vo <tuanvo.will@gmail.com>
2024-01-16 01:40:36 -0500

    implementing utility.py

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
-
-
 import tensorflow as tf
 import mediapipe as mp
 from pynput.keyboard import Key, Controller
-from mediapipe.tasks import python
-from mediapipe.tasks.python import vision
+import multiprocessing as _mp
+from utility import load_graph, detect_gesture, predict, racing_car
 import cv2
 import math
 
@@ -12,64 +10,21 @@ import math
 mp_drawing = mp.solutions.drawing_utils
 mp_drawing_styles = mp.solutions.drawing_styles
 mp_hands = mp.solutions.hands
-
-#pynbut
+# pynput
 keyboard = Controller()
+# set tf flags
+tf.flags.DEFINE_integer("width", 640, "Screen width")
+tf.flags.DEFINE_integer("height", 480, "Screen height")
+tf.flags.DEFINE_float("alpha", 0.3, "Transparent Level")
+FLAGS = tf.flag.FLAGS
 
 
-# For webcam input:
-cap = cv2.VideoCapture(0)
-
-with mp_hands.Hands(
-    model_complexity=0,
-    min_detection_confidence=0.5,
-    min_tracking_confidence=0.5) as hands:
-  while cap.isOpened():
-    success, image = cap.read()
-    if not success:
-      print("Ignoring empty camera frame.")
-      # If loading a video, use 'break' instead of 'continue'.
-      continue
-
-    # To improve performance, optionally mark the image as not writeable to
-    # pass by reference.
-    image.flags.writeable = False
-    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-    results = hands.process(image)
-
-    # Draw the hand annotations on the image.
-    image.flags.writeable = True
-    image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
-    
-    height, width, _ = image.shape ##
-    
-    if results.multi_hand_landmarks:
-      hand_centers = []
-      for hand_landmarks in results.multi_hand_landmarks:
-        hand_centers.append(
-          [int(hand_landmarks.landmark[9].x * width), 
-           int(hand_landmarks.landmark[9].y * height)]
-          )
-      
-        mp_drawing.draw_landmarks(
-            image,
-            hand_landmarks,
-            mp_hands.HAND_CONNECTIONS,
-            mp_drawing_styles.get_default_hand_landmarks_style(),
-            mp_drawing_styles.get_default_hand_connections_style())
-      if len(hand_centers) == 2:
-        cv2.line(image, (hand_centers[0][0], hand_centers[0][1]), (hand_centers[1][0], hand_centers[1][1]),
-                 (0,255,0), 5)
-        center_x = (hand_centers[0][0] + hand_centers[1][0]) // 2
-        center_y = (hand_centers[0][1] + hand_centers[1][1]) // 2
-        radius = int(math.sqrt((hand_centers[0][0] - hand_centers[1][0]) ** 2 +
-                               (hand_centers[0][1] - hand_centers[1][1]) ** 2) / 2)
-        cv2.circle(image, (center_x, center_y), radius, (0,255,0), 5)
-        ### moving foward:
-        keyboard.press('w')
-        
-    # Flip the image horizontally for a selfie-view display.
-    cv2.imshow('MediaPipe Hands', cv2.flip(image, 1))
-    if cv2.waitKey(5) & 0xFF == 27:
-      break
-cap.release()
+def main():
+    # graph, sess = load_graph(FLAGS.pre_trained_model_path)
+	cap = cv2.VideoCapture(0)
+	cap.set(cv2.CAP_PROP_FRAME_WIDTH, FLAGS.width)
+	cap.set(cv2.CAP_PROP_FRAME_HEIGHT, FLAGS.height)
+	mp = _mp.get_context("spawn")
+	v = mp.Value('i', 0)
+	lock = mp.Lock()
+	process = mp.Process(target=racing_car, args=(v, lock))
